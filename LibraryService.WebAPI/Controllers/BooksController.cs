134f8f7648818d1d35be2189c9181fa4bf2180b8
Milos Ivankovic <milos.ivankovic@nfinnova.com>
2025-01-27 14:38:06 +0100

    upload

---
+++ LibraryService.WebAPI/Controllers/BooksController.cs
@@ -0,0 +1,59 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using LibraryService.WebAPI.Data;
+using LibraryService.WebAPI.Services;
+
+namespace LibraryService.WebAPI.Controllers
+{
+    [ApiController]
+    [Route("api/libraries/{libraryId}/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly IBooksService _booksService;
+
+        public BooksController(IBooksService booksService)
+        {
+            _booksService = booksService;
+        }
+
+        [HttpGet("{libraryId}/{ints}")]
+        public async Task<IActionResult> Get(int libraryId, int[] ints)
+        {
+            var library = (await _booksService.Get(libraryId , ints)).FirstOrDefault();
+            if (library == null)
+                return NotFound();
+            return Ok(library);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(Book b)
+        {
+            await _booksService.Add(b);
+            return Ok(b);
+        }
+
+        [HttpPut("{libraryId}/{ints}")]
+        public async Task<IActionResult> Update(int libraryId, int[] ints, Book b)
+        {
+            var existingLibrary = (await _booksService.Get(libraryId, ints)).FirstOrDefault();
+            if (existingLibrary == null)
+                return NotFound();
+
+            await _booksService.Update(b);
+            return NoContent();
+        }
+
+        [HttpDelete("{libraryId}/{ints}")]
+        public async Task<IActionResult> Delete(int libraryId, int[] ints)
+        {
+            var library = (await _booksService.Get(libraryId, ints)).FirstOrDefault();
+            if (library == null)
+                return NotFound();
+
+            await _booksService.Delete(library);
+
+            return NoContent();
+        }
+    }
+}
---
+++ LibraryService.WebAPI/Services/BooksService.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryService.WebAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyModel;
+
+namespace LibraryService.WebAPI.Services
+{
+    public class BooksService : IBooksService
+    {
+        private readonly LibraryContext _libraryContext;
+
+        public BooksService(LibraryContext libraryContext)
+        {
+            _libraryContext = libraryContext;
+        }
+
+        public async Task<IEnumerable<Book>> Get(int libraryId, int[] ids)
+        {
+            var projects = _libraryContext.Books.AsQueryable();
+
+            if (ids != null && ids.Any())
+                projects = projects.Where(x => ids.Contains(x.Id));
+
+            return await projects.ToListAsync();
+        }
+
+        public async Task<Book> Add(Book book)
+        {
+            await _libraryContext.Books.AddAsync(book);
+
+            await _libraryContext.SaveChangesAsync();
+            return book;
+        }
+
+        public async Task<Book> Update(Book book)
+        {
+            // Complete the implementation
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> Delete(Book book)
+        {
+            // Complete the implementation
+            throw new NotImplementedException();
+        }
+    }
+
+    public interface IBooksService
+    {
+        Task<IEnumerable<Book>> Get(int libraryId, int[] ids);
+
+        Task<Book> Add(Book book);
+
+        Task<Book> Update(Book book);
+
+        Task<bool> Delete(Book book);
+    }
+}
